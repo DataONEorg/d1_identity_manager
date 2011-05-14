94e51a6a8f2760670164cc2c27b19b474beafff1
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2011-05-14 00:05:48 +0000

    add identity mapping unit test

--- src/test/java/org/dataone/service/cn/tier2/impl/CNIdentityLDAPImplTest.java
+++ src/test/java/org/dataone/service/cn/tier2/impl/CNIdentityLDAPImplTest.java
@@ -1,6 +1,7 @@
 package org.dataone.service.cn.tier2.impl;
 
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -110,4 +111,68 @@ public class CNIdentityLDAPImplTest {
 		}
 
     }
+
+	@Test
+	public void mapIdentity()  {
+	
+		try {
+			Principal p1 = new Principal();
+			p1.setValue(primaryPrincipal);
+			Person person1 = new Person();
+			person1.setPrincipal(p1);
+			person1.setFamilyName("test1");
+			person1.setGivenNames(Arrays.asList(new String[] {"test1"}));
+			
+			Principal p2 = new Principal();
+			p2.setValue(secondaryPrincipal);
+			Person person2 = new Person();
+			person2.setPrincipal(p2);
+			person2.setFamilyName("test2");
+			person2.setGivenNames(Arrays.asList(new String[] {"test1"}));
+			
+			CNIdentityLDAPImpl identityService = new CNIdentityLDAPImpl();		
+			boolean check = false;
+			
+			// create principals
+			Principal principal = identityService.registerAccount(person1);
+			assertNotNull(principal);
+			principal = identityService.registerAccount(person2);
+			assertNotNull(principal);
+			
+			// map p1 to p2
+			check = identityService.mapIdentity(p1, p2);
+			assertTrue(check);
+			// check pending
+			check = identityService.checkAttribute(p2, "equivalentIdentityRequest", p1.getValue());
+			assertTrue(check);
+			// request is one-way
+			check = identityService.checkAttribute(p1, "equivalentIdentityRequest", p2.getValue());
+			assertFalse(check);
+			// not yet confirmed on either end
+			check = identityService.checkAttribute(p1, "equivalentIdentity", p2.getValue());
+			assertFalse(check);
+			check = identityService.checkAttribute(p2, "equivalentIdentity", p1.getValue());
+			assertFalse(check);
+			// accept request
+			check = identityService.mapIdentity(p2, p1);
+			assertTrue(check);
+			
+			// double check reciprocal mapping
+			check = identityService.checkAttribute(p1, "equivalentIdentity", p2.getValue());
+			assertTrue(check);
+			check = identityService.checkAttribute(p2, "equivalentIdentity", p1.getValue());
+			assertTrue(check);
+			
+			// clean up (this is not required for service to be functioning)
+			check = identityService.removePrincipal(p1);
+			assertTrue(check);
+			check = identityService.removePrincipal(p2);
+			assertTrue(check);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	
+	}
 }
