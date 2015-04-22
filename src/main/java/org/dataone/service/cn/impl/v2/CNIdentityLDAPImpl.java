a6bb17579f955687d6f83bcfd2ee9862f772ab4b
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-04-22 16:51:05 +0000

    [again] Only look up one level of groups, equivalent identities and equivalent identity requests - otherwise we can end up recurising indefinitely when identity mappings return to the original subject after multiple hops. https://redmine.dataone.org/issues/7059

--- src/main/java/org/dataone/service/cn/impl/v2/CNIdentityLDAPImpl.java
+++ src/main/java/org/dataone/service/cn/impl/v2/CNIdentityLDAPImpl.java
@@ -989,8 +989,8 @@ public class CNIdentityLDAPImpl extends LDAPService implements CNIdentity {
 									}
 									// catch the NotFound in case we only have the subject's DN
 									try {
-										// recurse for equivalent identity requests
-										SubjectInfo equivalentIdentityRequestInfo = this.getSubjectInfo(null, equivalentIdentityRequest, true, originatingSubject);
+										// do not recurse for equivalent identity requests
+										SubjectInfo equivalentIdentityRequestInfo = this.getSubjectInfo(null, equivalentIdentityRequest, false, originatingSubject);
 										if (equivalentIdentityRequestInfo.getPersonList() != null) {
 											for (Person p: equivalentIdentityRequestInfo.getPersonList()) {
 												if (!contains(pList.getPersonList(), p)) {
@@ -1039,8 +1039,8 @@ public class CNIdentityLDAPImpl extends LDAPService implements CNIdentity {
 									}
 									// allow case where the identity is not found
 									try {
-										// recurse for equivalent identities
-										SubjectInfo equivalentIdentityInfo = this.getSubjectInfo(null, equivalentIdentity, true, originatingSubject);
+										// do not recurse for equivalent identities
+										SubjectInfo equivalentIdentityInfo = this.getSubjectInfo(null, equivalentIdentity, false, originatingSubject);
 										if (equivalentIdentityInfo.getPersonList() != null) {
 											for (Person p: equivalentIdentityInfo.getPersonList()) {
 												if (!contains(pList.getPersonList(), p)) {
