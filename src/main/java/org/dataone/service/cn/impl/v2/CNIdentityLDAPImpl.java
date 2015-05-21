40b057b95771a70b4319f6bc38d43846b8681173
Jing Tao <tao@nceas.ucsb.edu>
2015-05-21 23:21:01 +0000

    Add the code to enforce the start and count in the listSubject method.

--- src/main/java/org/dataone/service/cn/impl/v2/CNIdentityLDAPImpl.java
+++ src/main/java/org/dataone/service/cn/impl/v2/CNIdentityLDAPImpl.java
@@ -850,7 +850,9 @@ public class CNIdentityLDAPImpl extends LDAPService implements CNIdentity {
 
 		// check redaction policy
 		boolean redact = shouldRedact(session);
-		
+		if (start < 0) {
+		    start = 0;
+		}
 		SubjectInfo pList = new SubjectInfo();
 		try {
 			DirContext ctx = getContext();
@@ -883,7 +885,7 @@ public class CNIdentityLDAPImpl extends LDAPService implements CNIdentity {
 
 	        NamingEnumeration<SearchResult> results =
 	            ctx.search(base, searchCriteria, ctls);
-
+	        int index =0;
 	        while (results != null && results.hasMore()) {
 	            SearchResult si = results.next();
 	            String dn = si.getNameInNamespace();
@@ -897,13 +899,20 @@ public class CNIdentityLDAPImpl extends LDAPService implements CNIdentity {
                 	// add groups
 	                for (Group group: resultList.getGroupList()) {
 	                	if (!contains(pList.getGroupList(), group)) {
-	                		pList.addGroup(group);
+	                	    if(index >= start && index <= count) {
+	                	        pList.addGroup(group);
+	                	        index++;
+	                	    }
+	                		
 	                	}
 	                }
 	                // add people
 	                for (Person person: resultList.getPersonList()) {
 	                	if (!contains(pList.getPersonList(), person)) {
-	                		pList.addPerson(person);
+	                		if(index >= start && index <= count) {
+	                		    pList.addPerson(person);
+	                		    index++;
+                            }
 	                	}
 	                }
                 }
